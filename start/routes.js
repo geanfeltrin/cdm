beb350af9d57204ffb9444d3711fb2f27d6e7ee5
geanfeltrin <32302438+geanfeltrin@users.noreply.github.com>
2019-02-06 19:55:28 -0300

    Update routes.js

--- start/routes.js
+++ start/routes.js
@@ -17,8 +17,6 @@
 const Route = use('Route')
 
 Route.post('users', 'UserController.store')
-Route.get('users', 'UserController.index')
-
 Route.post('sessions', 'SessionController.store')
 
 Route.post('passwords', 'ForgotPasswordController.store')
