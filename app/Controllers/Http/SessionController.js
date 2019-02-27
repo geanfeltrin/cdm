b74323bccf0ac7445519817c65bcbabbcafb44cb
geanfeltrin <32302438+geanfeltrin@users.noreply.github.com>
2019-02-26 22:34:43 -0300

    test

--- app/Controllers/Http/SessionController.js
+++ app/Controllers/Http/SessionController.js
@@ -1,12 +1,6 @@
 'use strict'
 
-
 class SessionController {
-  async index ({ auth }) {
-    const user = await auth.getUser()
-    const roles = await user.getRoles()
-    return roles
-  }
   async store ({ request, response, auth }) {
     const { email, password } = request.all()
 
